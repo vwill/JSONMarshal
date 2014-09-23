12277f31f46fcbfc8f176562fce593b089507bd6
vaughn <vaughn.williams.za@me.com>
2014-09-23 19:50:57 +0200

    Update json.swift

--- JSONMarshal/json.swift
+++ JSONMarshal/json.swift
@@ -11,27 +11,26 @@ import Swift
 
 class json {
   
-  /**  Property types.
-  
-  - String, Double, Int, Bool: Standard Swift Types
-  - None: Unclassified or Unknown type
-  */
-  private enum PropertyType {
-    case String, Double, Int, Bool, None
-  }
-  
   /** Unmarshal parses the JSON-encoded data and stores the result in an array of a nominated class
   
+  Excerpt from Go:
   Unmarshal uses the inverse of the encodings that Marshal uses, allocating maps, slices, and pointers as necessary, with the following additional rules:
+  1. To unmarshal JSON into a pointer, Unmarshal first handles the case of the JSON being the JSON literal null. In that case, Unmarshal sets the pointer to nil. Otherwise, Unmarshal unmarshals the JSON into the value pointed at by the pointer. If the pointer is nil, Unmarshal allocates a new value for it to point to.
+  2. To unmarshal JSON into a struct, Unmarshal matches incoming object keys to the keys used by Marshal (either the struct field name or its tag), preferring an exact match but also accepting a case-insensitive match.
   
-  To unmarshal JSON into a pointer, Unmarshal first handles the case of the JSON being the JSON literal null. In that case, Unmarshal sets the pointer to nil. Otherwise, Unmarshal unmarshals the JSON into the value pointed at by the pointer. If the pointer is nil, Unmarshal allocates a new value for it to point to.
-  
-  To unmarshal JSON into a struct, Unmarshal matches incoming object keys to the keys used by Marshal (either the struct field name or its tag), preferring an exact match but also accepting a case-insensitive match.
+  Key differences with this Swift draft:
+  1. Currently returns NSMutableArray
+  2. No provision for complex structures (yet!)
+  3. A NSObject class is used as opposed to Struct re KVC
+  4. Tuple used for singular function to deal with multiple Type optional / conversion unpacking; I'm still trying my luck to simplify this with generics. Appreciate any help in this area (certainly not happy with the current plumbing)
   
   :marshalClass: nominated class structure to parse JSON data into
   :data: NSData byte buffer containing unparsed JSON
   
   */
+  
+  // TODO: - 1. Simplify Tuple / Switch / Type plumbing
+  // TODO: - 2. Explore complex class structure reflection parsing
   
   class func unMarshal(#marshalClass: NSObject.Type, data: NSData) -> NSMutableArray {
     
