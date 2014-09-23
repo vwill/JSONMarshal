cccd76f23e4126ca57ea20cb73ab1ea5ddc7e52c
Vaughn Williams <VW@vaughn-mbpro.local>
2014-09-23 15:21:58 +0200

    Initial Commit

---
+++ JSONMarshal/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  JSONMarshal
+//
+//  Created by Vaughn Williams on 2014/09/23.
+//  Copyright (c) 2014 Vaughn Williams. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
